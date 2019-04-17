13ae3b5fdc06fa4136c486b88e793e2729a5311c
Ashen Gunaratne <mail@ashenm.ml>
2019-04-17 07:59:11 +0000

    Configure CI/CD

---
+++ .travis/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# Deploy Jekyll Sidebar
+
+curl --fail --silent --show-error \
+  --request POST \
+  --output /dev/null \
+  --header "Content-Type: application/json" \
+  --header "Accept: application/json" \
+  --header "Travis-API-Version: 3" \
+  --header "Authorization: token $TRAVIS_API_TOKEN" \
+  --data "{ \"request\": { \"branch\": \"docs\", \"message\": \"Build to parity with commit `expr substr $TRAVIS_COMMIT 1 7` on $TRAVIS_BRANCH\" } }" \
+  --url "https://api.travis-ci.org/repo/ashenm%2Fjekyll-sidebar/requests"
